79d7d3ae863160e2b21e37ee6e6fd716e994aef7
Jeffrey Hall <halljeff@os1.engr.oregonstate.edu>
2019-01-13 15:29:23 -0800

    finished initial version of add.sh

--- v0-1/add.sh
+++ v0-1/add.sh
@@ -6,29 +6,35 @@ outfile="addresults"
 rowCount=4
 colCount=3
 
+tmp1="tmp_add1_$$"
+tmp2="tmp_add2_$$"
+tmp3="tmp_add3_$$"
+tmp4="tmp_add4_$$"
 
 for (( i="$rowCount"; i>0;i=i-1)) 
 do  
-    #get row number (rowcount - i) from file1, file2 -- (accessing from bottom)
-    r1="$( cat $infile1 | tail -n $i | head -n 1)"
-    r2="$( cat $infile2 | tail -n $i | head -n 1)"
-
-	echo -e "r1: $r1"
-	echo -e "r2: $r2"
-
+    #get single row, store in temp file
+    cat $infile1 | tail -n $i | head -n 1 > $tmp1
+    cat $infile2 | tail -n $i | head -n 1 > $tmp2
 
     for (( j=1; j <= $colCount;j=j+1)) 
     do
-        #get the jth element from rows r1, r2
-        #e1="$( cut -f $j $r1 )"
-        #e2="$( cut -f $j $r2 )"
-        #echo "e1:$e1 e2:$e2" 
-        
-	cut -f $j $r1
-        cut -f $j $r2
+        #extract jth element of row into e1, e2
+        e1=$(cut -f $j $tmp1)
+        e2=$(cut -f $j $tmp2)
 
-        # outputf
-        #cat $( $e1 + $e2 ) 
+        #tmp3 containing current summed row elements separated by '\n'
+        expr $e1 + $e2  >> $tmp3
     done
 
+	#tmp4 contains final result matrix	
+    cat $tmp3 | tr '\n' '\t' >> $tmp4
+    rm $tmp3
+
+    echo "" >> $tmp4
+	 
 done
+
+cat $tmp4
+
+rm $tmp1 $tmp2 $tmp4
